f64710d4fc9a3a8346a3b704e862e9537d65ce1c
DarkSnake-Kobra <jbttoms@gmail.com>
2016-11-23 02:15:08 -0600

    Signed-off-by: DarkSnake-Kobra <jbttoms@gmail.com>

---
+++ scripts/footer.js
@@ -0,0 +1,7 @@
+function makeFooter()
+{
+	document.write('<p><a href="index.html">Home</a> - ');
+	document.write('<a href="about.html">About</a> - ');
+	document.write('<a href="sitemap.html">Site Map</a>');
+	document.write('</br>Copyright (C) 2016 Jordan Bottoms</p>');
+}
